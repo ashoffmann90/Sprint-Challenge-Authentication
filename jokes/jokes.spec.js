06d1e369461d82f55be12f00f4a78cdf0200c53c
Andrew Hoffmann <ashoffmann90@gmail.com>
2020-05-22 11:47:53 -0700

    MVP

--- jokes/jokes.spec.js
+++ jokes/jokes.spec.js
@@ -4,35 +4,46 @@ const db = require( '../database/dbConfig')
 
 beforeEach(() => {
     return db.migrate
-      .rollback()
-      .then(() => db.migrate.latest())
-      .then(() => db.seed.run());
-  });
+    .rollback()
+    .then(() => db.migrate.latest())
+    .then(() => db.seed.run());
+});
 
-describe('jokes', () => {
-    it('runs the tests', () => {
-        expect(true).toBeTruthy();
-    })
-
-    describe('GET /jokes', () => {
-        it('returns status 200', async() => {
-            const reg = await request(server)
-            .post('/api/auth/register')
-            .send({ 
-                username: 'dude',
-                password: 'test'
-            })
-            const res = await request(server)
-            .post('/api/auth/login')
-            .send({
-                username: 'dude', 
-                password: 'test'
-            })
-            const getJokes = await request(server)
-            .get('/api/jokes')
-            console.log(getJokes.status)
-            expect(getJokes.status).toBe(200)
-        })
-
-    })
+test('runs the tests', () => {
+    expect(true).toBeTruthy();
 })
+    
+test('get to /jokes returns 400 without auth', async ()=> {
+    const res = await request(server)
+    .get('/api/jokes')
+    console.log(res)
+    expect(res.status).toBe(400)
+})
+
+test('get to /jokes returns an error saying you need authentication information', async () => {
+    const res = await request(server)
+    .get('/api/jokes')
+    expect(res.error.text).toBe('{"error":"Please provide authentication information"}')
+})
+
+
+
+// test with auth
+    // it('returns status 200', async() => {
+    //     const reg = await request(server)
+    //     .post('/api/auth/register')
+    //     .send({ 
+    //         username: 'dude',
+    //         password: 'test'
+    //     })
+    //     const res = await request(server)
+    //     .post('/api/auth/login')
+    //     .send({
+    //         username: 'dude', 
+    //         password: 'test'
+    //     })
+    //     const getJokes = await request(server)
+    //     .get('/api/jokes')
+    //     console.log(getJokes.status)
+    //     expect(getJokes.status).toBe(200)
+    // })
